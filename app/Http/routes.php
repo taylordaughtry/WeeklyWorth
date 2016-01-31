b5b1facb3fdc647d2f34c9e760cabcb2fe19daad
Taylor Daughtry <taylor@caddis.co>
2016-01-31 11:28:41 -0600

    Add Auth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,20 @@ Route::get('/', 'PagesController@index')->name('home');
 Route::resource('income', 'IncomesController');
 Route::resource('expense', 'ExpensesController');
 Route::resource('company', 'CompaniesController');
+
+
+/*------------------------------------*\
+	# Authentication
+\*------------------------------------*/
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+
+/*------------------------------------*\
+	# Registration
+\*------------------------------------*/
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
